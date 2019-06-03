8ceb88043df02aeab42b2d45f641237578103314
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-06-03 11:22:52 -0300

    fix pem config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 // config.js
 module.exports = {
-  privateKey: process.env.PRIVATE_KEY,
-  publicKey: process.env.PUBLIC_KEY,
-  refreshTokenPublicKey: process.env.REFRESH_TOKEN_PUBLIC_KEY,
-  refreshTokenPrivateKey: process.env.REFRESH_TOKEN_PRIVATE_KEY,
+  privateKey: JSON.parse(process.env.PRIVATE_KEY),
+  publicKey: JSON.parse(process.env.PUBLIC_KEY),
+  refreshTokenPublicKey: JSON.parse(process.env.REFRESH_TOKEN_PUBLIC_KEY),
+  refreshTokenPrivateKey: JSON.parse(process.env.REFRESH_TOKEN_PRIVATE_KEY),
   mongodbUrl: process.env.MONGODB_URL,
   mongodbUrlTest: process.env.MONGODB_URL_TEST,
   googleApiKey: process.env.GOOGLE_API_KEY
